2d8326ca52ff9650753088e9b601de024cf14441
tjforeman <tylerforeman92@gmail.com>
2019-12-06 13:52:46 -0500

    ex2 passing tests

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -19,5 +19,12 @@ def reconstruct_trip(tickets, length):
     """
     YOUR CODE HERE
     """
+    for i in range(length):
+        hash_table_insert(hashtable, tickets[i].source, tickets[i].destination)
 
+    for i in range(length):
+        if i == 0:
+            route[i] = hash_table_retrieve(hashtable, 'NONE')
+        else:
+            route[i] = hash_table_retrieve(hashtable, route[i-1])
     return route
